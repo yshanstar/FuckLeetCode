26ee17cb1db1e7d6a9e896e0da0c1a9941c7def9
Shan Ye <Shan Ye@shye-win10>
2016-08-26 20:37:20 -0700

    edit comments

--- src/hack/google/BinaryTreeLongestConsecutiveSequence.java
+++ src/hack/google/BinaryTreeLongestConsecutiveSequence.java
@@ -24,6 +24,7 @@ Longest consecutive sequence path is 3-4-5, so return 3.
   / 
  1
 Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+//
  */
 public class BinaryTreeLongestConsecutiveSequence {
 	public int longestConsecutive(TreeNode root) {
