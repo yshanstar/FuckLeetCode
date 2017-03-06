fd30b73cab28661d05a10a7625949cc4e906784d
Shan Ye <ye.shan@oracle.com>
2017-03-05 22:23:09 -0800

    Continuous Subarray Sum

---
+++ src/hack/facebook/ContinuousSubarraySum.java
@@ -0,0 +1,45 @@
+package hack.facebook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+ * <p>
+ * Example 1:
+ * Input: [23, 2, 4, 6, 7],  k=6
+ * Output: True
+ * Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+ * Example 2:
+ * Input: [23, 2, 6, 4, 7],  k=6
+ * Output: True
+ * Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ * Note:
+ * The length of the array won't exceed 10,000.
+ * You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+ */
+public class ContinuousSubarraySum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int runningSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            runningSum += nums[i];
+
+            if (k != 0) {
+                runningSum %= k;
+            }
+
+            Integer previous = map.get(runningSum);
+            if (previous == null) {
+                map.put(runningSum, i);
+            } else {
+                if (i - previous > 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
