e348c618ecbb4a19ce780b5c47b875dc16021e88
Shan Ye <ye.shan@oracle.com>
2018-07-30 23:56:39 -0700

    865. Smallest Subtree with all the Deepest Nodes

--- src/hack/util/TreeNode.java
+++ src/hack/util/TreeNode.java
@@ -1,13 +1,13 @@
 package hack.util;
 
 public class TreeNode {
-	public int val;
-	public TreeNode left;
-	public TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-	public TreeNode(int v) {
-		this.val = v;
-		this.left = null;
-		this.right = null;
-	}
+    public TreeNode(int v) {
+        this.val = v;
+        this.left = null;
+        this.right = null;
+    }
 }
