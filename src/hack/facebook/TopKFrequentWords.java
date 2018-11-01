13b3e9af0e05e4b4bd9c87ae866e78d4c8ec6fac
Shan Ye <ye.shan@oracle.com>
2018-10-31 21:08:01 -0700

    692.Top K Frequent Words

---
+++ src/hack/facebook/TopKFrequentWords.java
@@ -0,0 +1,61 @@
+package hack.facebook;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+/*
+Given a non-empty list of words, return the k most frequent elements.
+
+Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+Example 1:
+Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+Output: ["i", "love"]
+Explanation: "i" and "love" are the two most frequent words.
+    Note that "i" comes before "love" due to a lower alphabetical order.
+Example 2:
+Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+Output: ["the", "is", "sunny", "day"]
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+    with the number of occurrence being 4, 3, 2 and 1 respectively.
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Input words contain only lowercase letters.
+Follow up:
+Try to solve it in O(n log k) time and O(n) extra space.
+
+Reference:
+https://leetcode.com/problems/top-k-frequent-words/discuss/108346/My-simple-Java-solution-using-HashMap-and-PriorityQueue-O(nlogk)-time-and-O(n)-space
+
+ */
+public class TopKFrequentWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.putIfAbsent(word, 0);
+            map.put(word, map.get(word) + 1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> priorityQueue = new PriorityQueue<>(
+                (a, b) -> Objects.equals(a.getValue(), b.getValue()) ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue()
+        );
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            priorityQueue.offer(entry);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            result.add(0, Objects.requireNonNull(priorityQueue.poll()).getKey());
+        }
+        return result;
+    }
+}
