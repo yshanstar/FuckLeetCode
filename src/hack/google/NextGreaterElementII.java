1e0792e2f61e0831b32dd89575617f0be9db8421
Shan Ye <ye.shan@oracle.com>
2018-11-04 17:24:03 -0800

    503.Next Greater Element II

---
+++ src/hack/google/NextGreaterElementII.java
@@ -0,0 +1,41 @@
+package hack.google;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element.
+The Next Greater Number of a number x is the first greater number to its traversing-order next in the array,
+which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+
+Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2;
+The number 2 can't find next greater number;
+The second 1's next greater number needs to search circularly, which is also 2.
+Note: The length of given array won't exceed 10000.
+
+
+ */
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int length = nums.length;
+        int next[] = new int[length];
+        Arrays.fill(next, -1);
+
+        // stack is for index
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * length; i++) {
+            int num = nums[i % length];
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                next[stack.pop()] = num;
+            }
+            if (i < length) {
+                stack.push(i);
+            }
+        }
+
+        return next;
+    }
+}
